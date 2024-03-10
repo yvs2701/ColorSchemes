31812c36fba295ece88b10a8696aaa7963e0e589
yvs2701 <yashvardhan.ys86@gmail.com>
2024-03-10 11:31:27 +0530

    added mock api server

--- frontend/tests/App.test.jsx
+++ frontend/tests/App.test.jsx
@@ -1,10 +1,8 @@
-import { render, screen, waitFor, waitForElementToBeRemoved } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import userEvent from '@testing-library/user-event'
 import { test, expect } from "vitest"
 import { routesConfig } from "../src/App.jsx"
 import { RouterProvider, createMemoryRouter } from "react-router-dom"
-
-///NOTE - Run the flask server before running these tests on the development react app
 
 let COLOR_CODE, route;
 
@@ -32,7 +30,7 @@ test('Render ColorExtractor component', async () => {
   await user.click(element)
   await screen.findByRole('heading', { name: /pick a color/i }, { timeout: 2000 }) // wait for maximum 2 seconds for the page to load
   // if above line throws no error then test passes as we navigate to the next component
-  
+
   route = element.getAttribute('href')
   console.log('Navigated to: ' + route)
 })
@@ -40,9 +38,19 @@ test('Render ColorExtractor component', async () => {
 test('Render ColorTheme component', async () => {
   render(<RouterProvider router={createMemoryRouter(routesConfig, { initialEntries: [route] })} />)
   await screen.findByRole('heading', { name: /pick a color/i }) // by default wait for maximum 1 second for the page to load
-  
+
   ///NOTE - this page has now been loaded
   const btns = screen.getAllByRole('button', { name: /^#(?:[0-9a-fA-F]{3}){1,2}$/ })
   console.log('Primary color of the generated theme: ' + btns[0].innerHTML);
   expect(btns[0].innerHTML === COLOR_CODE).toBe(true)
 })
+
+/* test('Snapshot Homepage', () => {
+  const router = createMemoryRouter(routesConfig, {
+    initialEntries: ["/"],
+  })
+
+  const page = render(<RouterProvider router={router} />)
+
+  expect(page).toMatchSnapshot()
+}) */
--- frontend/tests/setup.js
+++ frontend/tests/setup.js
@@ -1,8 +1,42 @@
-import { afterEach } from 'vitest'
+import { afterAll, afterEach, beforeAll } from 'vitest'
 import { cleanup } from '@testing-library/react'
+import { HttpResponse, http } from 'msw'
+import { setupServer } from 'msw/node'
 import '@testing-library/jest-dom/vitest'
 
-// runs a clean after each test case (e.g. clearing jsdom)
-afterEach(() => {
-  cleanup();
-})
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+console.log('Backend URL: ' + BACKEND_URL);
+
+const extractedColors = {
+  "hex_colors": ["#223e51", "#ccd6ed", "#737a88", "#a6c5dd", "#eae1ee"],
+  "rgb_colors": [[34, 62, 81], [204, 214, 237], [115, 122, 136], [166, 197, 221], [234, 225, 238]],
+}
+
+const generatedColors = {
+  "hex_colors": [
+    ["primary_color", "#ccd6ed"], ["secondary_color", "#c2b084"], ["primary_tint", "#ffffff"],
+    ["secondary_tint", "#ffffff"], ["primary_shadow", "#322ead"], ["secondary_shadow", "#717330"]
+  ],
+  "rgb_colors": [
+    ["primary_color", [204, 214, 237]], ["secondary_color", [194, 176, 132]], ["primary_tint", [255, 255, 255]],
+    ["secondary_tint", [255, 255, 255]], ["primary_shadow", [50, 46, 173]], ["secondary_shadow", [113, 115, 48]]
+  ]
+}
+
+export const restHandlers = [
+  http.post(`${BACKEND_URL}/api/extract_colors`, () => {
+    return HttpResponse.json(extractedColors)
+  }),
+  http.post(`${BACKEND_URL}/api/generate_colors`, () => {
+    return HttpResponse.json(generatedColors)
+  }),
+]
+
+const server = setupServer(...restHandlers)
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
+
+afterAll(() => server.close())
+
+// unmount component after each test
+afterEach(() => cleanup())
